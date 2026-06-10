9a81584c57cde626807cb18df9d166d5e25e8fb7
mondol2023 <abm20154@gmail.com>
2026-06-10 23:22:32 +0600

    settings fixed2

--- paid_parking_system_backend/settings.py
+++ paid_parking_system_backend/settings.py
@@ -67,17 +67,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
 ]
- """
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'rest_framework.middleware.CsrfViewMiddleware',
-    """
+ 
 
 CORS_ALLOW_ALL_ORIGINS = True
 
